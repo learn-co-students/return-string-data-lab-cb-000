04c76e95905f91d712f03ac6946c9d3b9e8d7259
joel <joelbitar@protonmail.com>
2017-06-30 14:47:16 -0400

    Done.

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -1,6 +1,8 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+    	t.string :description
+    	t.integer :inventory
       t.string :name
       t.integer :price
       t.timestamps null: false
