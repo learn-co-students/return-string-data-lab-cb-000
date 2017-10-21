799a5666fc7720f36154dd8797b0cb3b036c564d
tahbristol <tahbristol@gmail.com>
2017-10-20 22:48:38 -0400

    step 3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,16 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+   def new
+    @product = Product.new
+   end
+
+   def create
+     binding.pry
+     @product = Product.create(name: params[:product][:name], description: params[:product][:description], inventory: params[:product][:inventory])
+     redirect_to products_path
+   end
+
   def description
     product = Product.find(params[:id])
     render plain: product.description
