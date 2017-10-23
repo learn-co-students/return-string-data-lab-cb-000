51e7a0f781d8b4ac71e1a3f45cb1704aa67a2bec
github username <github email address>
2017-10-23 10:51:50 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,41 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :description, :inventory]
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def description
+    # @product_description = @product.description
+    render plain: @product.description
+  end
+
+  def inventory
+    render plain: @product.inventory > 0 ? "true" : "false"
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :inventory, :price)
+    end
 end
--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -2,7 +2,10 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.text :description
+      t.integer :inventory
       t.integer :price
+
       t.timestamps null: false
     end
   end
