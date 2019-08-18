e0f83bbb14ea95baf879359b21a4409186374149
github username <github email address>
2019-08-18 15:09:38 -0400

    Done.

---
+++ app/models/description.rb
@@ -0,0 +1,4 @@
+class Description < ActiveRecord::Base
+    belongs_to :Product 
+end
+  
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
-  has_many :ordered_products
-  has_many :orders, :through => :ordered_products
+    has_many :ordered_products 
+    has_many :orders, :through => :ordered_products 
 end
+  
+
+
+# RSpec.describe Product, type: :model do
+#     it { should respond_to :description }
+#     it { should respond_to :inventory }
+#   end
