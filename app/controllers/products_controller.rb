06706910f959ee34784cb15289715d11ecb63488
Will Hodgson <will.hodgson@current-rms.com>
2019-11-27 16:41:02 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,27 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.create(params.require(:product).permit(:name, :description, :inventory))
+    redirect_to products_path
+  end
+
+  def show
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
 end
