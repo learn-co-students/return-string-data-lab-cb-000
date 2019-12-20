1d6a0967a4baf3256ff622dcdcde1ee8fb488c55
Efua Mensah-Brown <emensahb@efbelles-laptop.lippuner.ca>
2019-12-20 11:59:18 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,45 @@
 class ProductsController < ApplicationController
 
-end
+   def index
+     @products = Product.all
+   end
+
+   def description
+    product = Product.find(params[:id])
+    render plain: product.description
+   end
+
+   def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+   def new
+     @product = Product.new
+   end
+
+   def create
+     @product = Product.create(product_params)
+     @product.save
+     redirect_to products_path
+   end
+
+   def edit
+   end
+
+   def update
+     @product.update(product_params)
+     redirect_to product_path(@product)
+   end
+
+ private
+   # Use callbacks to share common setup or constraints between actions.
+   def set_product
+     @product = Product.find(params[:id])
+   end
+
+   # Never trust parameters from the scary internet, only allow the white list through.
+   def product_params
+     params.require(:product).permit(:name, :price,:description, :inventory)
+   end
+ end
