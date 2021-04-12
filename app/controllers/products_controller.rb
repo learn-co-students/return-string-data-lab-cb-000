fc5635c5c7d8c42f64efe8eb09866552c7b5364e
AndreaJasper <andrea.nicole.jasper@gmail.com>
2021-04-12 08:46:58 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,28 @@
 class ProductsController < ApplicationController
 
+  def description
+    productd = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    productd = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
 end
